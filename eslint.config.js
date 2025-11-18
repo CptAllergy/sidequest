fb231dd7fe47fb7c4feccc7f976b8313169c0ef1
Gonçalo Prates <49039994+CptAllergy@users.noreply.github.com>
2025-11-18 22:37:24 +0000

    Fixed some linting errors and added react query and shadcn

--- eslint.config.js
+++ eslint.config.js
@@ -3,4 +3,17 @@
 import { tanstackConfig } from "@tanstack/eslint-config";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default [...tanstackConfig, eslintPluginPrettierRecommended];
+// TODO can this config be simplified?
+export default [
+  ...tanstackConfig,
+  eslintPluginPrettierRecommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      parserOptions: {
+        project: false,
+        projectService: { allowDefaultProject: ["*.js"] },
+      },
+    },
+  },
+];
